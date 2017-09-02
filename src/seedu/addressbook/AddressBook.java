4544fc78b76d1823fbd0e345a3a13a09639f5d14
Joshua <joshua.nkj@gmail.com>
2017-09-02 15:36:12 +0800

    Testing commit revert

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -1,5 +1,5 @@
 package seedu.addressbook;
-
+//Testing commit
 /*
  * NOTE : =============================================================
  * This class is written in a procedural fashion (i.e. not Object-Oriented)
